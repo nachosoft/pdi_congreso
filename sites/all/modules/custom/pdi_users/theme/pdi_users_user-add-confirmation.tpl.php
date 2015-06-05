bce56766fbe72736f240b51c749bb09d7154ca61
vipomx <vipopmx@gmail.com>
2015-06-05 05:58:55 -0600

    added  pdi users custom module

---
+++ sites/all/modules/custom/pdi_users/theme/pdi_users_user-add-confirmation.tpl.php
@@ -0,0 +1,6 @@
+<h2>El nuevo operador fue agregado con exito.</h2>
+<p>El nuevo operador recibira un email con las instrucciones para iniciar sesion.</p>
+</br>
+<div class="cta">
+  <a href="/pdi/users/add">Agregar otro operador?</a>
+</div>
